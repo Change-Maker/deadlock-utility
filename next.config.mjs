84b3bdfff74d24a99bffbdc01ae7f7bffd48a212
Enigma <chaosdelegation@gmail.com>
2024-09-11 14:28:57 +0800

    build: configure next.js
    - Set base path which is the slug of the GitHub repository.
    - Enable static exports.
    - Disable server-based image optimization.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: "/deadlock-utility",
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
